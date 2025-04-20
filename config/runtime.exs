facd9a0c83a47ff2ab62e5a967810c6a4f3c9689
aurmartin <aurmartin@pm.me>
2025-04-20 13:55:20 +0200

    build: fix server startup

--- config/runtime.exs
+++ config/runtime.exs
@@ -50,19 +50,14 @@ if config_env() == :prod do
 
   host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
+  ip_string = System.get_env("BIND_IP") || "127.0.0.1"
+  {:ok, ip} = :inet.parse_address('#{ip_string}')
 
   config :extick, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :extick, ExtickWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {127, 0, 0, 1},
-      port: port
-    ],
+    http: [ip: ip, port: port],
     secret_key_base: secret_key_base
 
   # ## SSL Support
